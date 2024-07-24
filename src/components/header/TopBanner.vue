6ccc011462b1df26ba828e0056eca8fb1e24f770
dnflboy <dnflboy@gmail.com>
2024-07-24 15:30:19 +0900

    7월24일 1차

---
+++ src/components/header/TopBanner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <img alt="Vue logo" src="./assets/image/banner/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopBanner',
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+  
+</style>
